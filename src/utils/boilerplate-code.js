30e282599f37ecf0af4b288368180622941759ab
Denis Efremov <efremov.a.denis@gmail.com>
2018-01-28 17:36:05 +0300

    Fixed bpCode

--- src/utils/boilerplate-code.js
+++ src/utils/boilerplate-code.js
@@ -2,17 +2,12 @@ const debug = require('debug')('js-bot:boilerplate-code')
 const et = require('./escape-tilde')
 
 
-function boilerplateGenerated(code) {
-  debug('template formated')
-  return `
-    const vm = require('vm')
-    const sandbox = require('../src/utils/context')
-
-    vm.createContext(sandbox)
-    vm.runInContext(\`${et(code)}\`, sandbox)
-  `
-}
-
 module.exports = {
-  boilerplateGenerated,
+  boilerplateGenerated(code) {
+    debug('Template formating started')
+    return `const vm = require('vm')
+const sandbox = require('../src/utils/context')
+vm.createContext(sandbox)
+vm.runInContext(\`${et(code)}\`, sandbox)`
+  },
 }
